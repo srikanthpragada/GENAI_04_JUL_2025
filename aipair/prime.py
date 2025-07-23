0cd9ce6da0ff13a2babbb71c4db4b8cf3385a365
srikanthpragada <srikanthpragada@gmail.com>
2025-07-23 20:56:19 +0530

    23-JUL

---
+++ aipair/prime.py
@@ -0,0 +1,22 @@
+def isprime(num):
+    """Check if a number is prime."""
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+# Function to calculate factorial of the given number 
+def factorial(n):
+    """Calculate the factorial of a number."""
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+    
